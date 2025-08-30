c6fac3140800c924e0b56168570cb62a10a69ccc
Taysa Abinader <ferreira.taysa@gmail.com>
2025-08-30 19:01:47 +0300

    Review Task 3

--- OrientationChapter_1_1/Customer.java
+++ OrientationChapter_1_1/Customer.java
@@ -1,23 +1,29 @@
+import java.util.concurrent.TimeUnit;
 
 public class Customer {
     private static int idIncrementor = 1;
     private int id;
-    private long startTime;
-    private long endTime;
+    private long startTime = -1;
+    private long endTime = -1;
 
     public Customer () {
         this.id = idIncrementor++;
-        this.startTime = System.currentTimeMillis();
-        this.endTime = System.currentTimeMillis() + 2000;
     }
 
     public int getId () {
         return this.id;
     }
 
+    public void updateStartTime() {
+        this.startTime = System.currentTimeMillis();
+    }
 
     public long getStartTime () {
         return this.startTime;
+    }
+
+    public void updateEndTime() {
+        this.endTime = System.currentTimeMillis();
     }
 
     public long getEndTime () {
@@ -32,26 +38,31 @@ public class Customer {
 
     public static void main(String[] args) {
         Customer customerName1 = new Customer();
-        customerName1.getStartTime();
+        customerName1.updateStartTime();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         Customer customerName2 = new Customer();
-        customerName2.getStartTime();
+        customerName2.updateStartTime();
 
-        
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        customerName1.getEndTime();
+        customerName1.updateEndTime();
         customerName1.timeSpentInSeconds();
 
-        customerName2.getEndTime();
+        customerName2.updateEndTime();
         customerName2.timeSpentInSeconds();
 
         System.out.println("Id: " + customerName1.id + ", time spent: " + customerName1.timeSpentInSeconds());
         System.out.println("Id: " + customerName2.id + ", time spent: " + customerName2.timeSpentInSeconds());
-        
-
-
-        
-        
     }
 
 }
