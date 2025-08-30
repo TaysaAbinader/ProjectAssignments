d1aa92c40d13da0c020c4f3ea5fdf05aceacd5e6
Taysa Abinader <ferreira.taysa@gmail.com>
2025-08-30 20:58:16 +0300

    Review Task 4

--- OrientationChapter_1_1/Queue.java
+++ OrientationChapter_1_1/Queue.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.Scanner;
 
 
 public class Queue {
@@ -13,12 +14,13 @@ public class Queue {
         queueLinkedList.addFirst(customer);
     }
 
-    public void leaveQueue () {
+    public Customer leaveQueue () {
         if (queueLinkedList.isEmpty()) {
             System.err.println ("Error: Queue is empty");
         }
         Customer removedCustomer = queueLinkedList.removeLast();
         removedCustomer.updateEndTime();
+        return removedCustomer;
     }
 
     public String toString() {
@@ -28,37 +30,36 @@ public class Queue {
     public static void main(String[] args) {
 
         Queue queue = new Queue();
+        Scanner scanner = new Scanner(System.in);
 
-        Customer customer1 = new Customer();
-        Customer customer2 = new Customer();
-        Customer customer3 = new Customer();
-        Customer customer4 = new Customer();
-        Customer customer5 = new Customer();
+        System.out.println("Accepted options:");
+        System.out.println("- \"queue\": Add a new customer to the queue");
+        System.out.println("- \"dequeue\": Remove the oldest customer from the queue");
+        System.out.println("- \"list\": List all customers in the queue");
+        System.out.println("- \"exit\": Exit program");
 
-        queue.enterQueue(customer1);
-        queue.enterQueue(customer2);
-        queue.enterQueue(customer3);
-
-        System.out.println(queue);
-
-        queue.leaveQueue();
-
-        queue.enterQueue(customer4);
-        queue.enterQueue(customer5);
-
-        System.out.println(queue);
-
-        queue.leaveQueue();
-        queue.leaveQueue();
-
-        System.out.println(queue);
-
-        System.out.println("Waiting times:");
-        System.out.println("Id: " + customer1.getId() + ", time spent: " + customer1.timeSpent() + " nanoseconds");
-        System.out.println("Id: " + customer2.getId() + ", time spent: " + customer2.timeSpent() + " nanoseconds");
-        System.out.println("Id: " + customer3.getId() + ", time spent: " + customer3.timeSpent() + " nanoseconds");
+        // Loop
+        while (true) {
+            System.out.println("What do you want to do?");
+            String action = scanner.nextLine();
+            if (action.equals("queue")) {
+                Customer newCustomer = new Customer();
+                System.out.println("Added customer " + newCustomer.getId() + " to the queue");
+                queue.enterQueue(newCustomer);
+            } else if (action.equals("dequeue")) {
+                Customer removedCustomer = queue.leaveQueue();
+                System.out.println("Removed customer " + removedCustomer.getId() + " from the queue");
+                System.out.println("Time spent: " + removedCustomer.timeSpent() + " ns");
+            } else if (action.equals("list")) {
+                System.out.println(queue.toString());
+            } else if (action.equals("exit")) {
+                System.out.println("Exiting...");
+                break;
+            } else {
+                System.err.println("Error: wrong command \"" + action + '\'');
+            }
+        }
     }
-
 }
 
 
